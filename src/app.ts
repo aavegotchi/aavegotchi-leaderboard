766a3ce521166d85543129b09285a95bcb1278ac
candoizo <candoizo@protonmail.com>
2022-08-06 10:52:22 -0400

    export, tiebreaker, sets logic

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import debug from 'debug';
+import jetpack from 'fs-jetpack';
 import { gql, GraphQLClient } from 'graphql-request';
 import setInfo from './setList.js';
 
@@ -7,24 +8,20 @@ if (!process.env.DEBUG) process.exit(1);
 
 const log = debug(process.env.DEBUG);
 
+const winnerRange = 7_500;
+const season = 3;
+const round = 4;
+const block_number = 27404025;
 const subgraphUrl =
 	'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic';
 
 const client = new GraphQLClient(subgraphUrl);
 
-const winnerRange = 7_500;
-const round = 1;
-
-const traitRoundTiebreaker = (traitsA: number[], traitsB: number[]) => {
-	const tiebreakerIndex = round - 1;
-	return traitsA[tiebreakerIndex] - traitsB[tiebreakerIndex];
-};
-
 const allGotchiInfo = async () => {
 	const maxSupply = 25_000;
 	let queryStr = '';
 	for (let i = 0; i < maxSupply / 1_000; i++) {
-		queryStr += gql` data${i}:aavegotchis(first:1000 where: {
+		queryStr += gql` data${i}:aavegotchis(block: {number: ${block_number}} first:1000 where: {
             id_gt: ${i * 1_000},
             id_lte: ${i * 1000 + 1000}
             baseRarityScore_gt: 0
@@ -39,17 +36,22 @@ const allGotchiInfo = async () => {
 	}
 
 	const info = await client.request(gql`{${queryStr}}`);
-	return Object.fromEntries(
-		Object.values(info)
-			.flat()
-			// @ts-ignore
-			.map((data) => [data.id, data]),
-	);
+	const gotchis: {
+		id: number;
+		kinship: number;
+		equippedWearables: number[];
+		experience: number;
+		baseRarityScore: number;
+		modifiedRarityScore: number;
+		modifiedNumericTraits: number[];
+	}[][] = Object.values(info);
+	return Object.fromEntries(gotchis.flat().map((data) => [data.id, data]));
 };
 
 const allSetsForItems = (items: number[]): number[] => {
 	const matchingSets = setInfo
 		.map(({ wearableIds }, index) => {
+			// ! @TODO
 			const includesSet = wearableIds.every((num) => items.includes(num));
 			if (includesSet) return index;
 			return 0;
@@ -96,86 +98,110 @@ const rarityScoreBonus = (traits: number[]): number => {
 
 const main = async () => {
 	const gotchiData = await allGotchiInfo();
-	log(`gotchi res`, gotchiData);
 
 	const gotchiToSets = Object.fromEntries(
 		Object.entries(gotchiData).map((info) => {
-			// @ts-ignore
 			const sets = allSetsForItems(info[1].equippedWearables);
-			const best = bestSetOfSets(sets);
-			if (sets.length > 1) log(`best set was: `, best, `of`, sets);
 			return [info[0], sets];
 		}),
 	);
 
 	const gotchiBestSetTraitsRarityScore = Object.fromEntries(
 		Object.entries(gotchiToSets).map(([key, sets]) => {
+			const currentGotchiInfo = gotchiData[key];
 			if (sets.length == 0)
 				return [
 					key,
 					{
-						id: key,
-						finalRarityScore: gotchiData[key].modifiedRarityScore,
-						finalTraits: gotchiData[key].modifiedNumericTraits,
+						...currentGotchiInfo,
+						rarity: gotchiData[key].modifiedRarityScore,
+						setTraits: gotchiData[key].modifiedNumericTraits,
 					},
 				];
 
 			const best = bestSetOfSets(sets);
 			const bestSetInfo = setInfo[best];
-			const bestSetBrsBoost = bestSetInfo.traitsBonuses[0];
-
-			const currentGotchiInfo = gotchiData[key];
+			const bestSetTraitBoots = bestSetInfo.traitsBonuses.slice(
+				1,
+				bestSetInfo.traitsBonuses.length,
+			);
 
 			const gotchiSetFinalTraits = currentGotchiInfo.modifiedNumericTraits.map(
 				(trait: number, index: number) => {
 					if (index >= 4) return trait;
-					// traitsBonuses: [brs, nrg, agg, spk, brn]
-					return trait + bestSetInfo.traitsBonuses[index + 1];
+					return trait + bestSetTraitBoots[index];
 				},
 			);
 
-			// ! @TODO: boostDifference?
+			const setsTraitsRarityScore = rarityScoreBonus(gotchiSetFinalTraits);
+			const bonusDifference =
+				setsTraitsRarityScore -
+				rarityScoreBonus(currentGotchiInfo.modifiedNumericTraits);
 
-			const gotchiSetFinalRarityScore =
-				rarityScoreBonus(gotchiSetFinalTraits) + bestSetBrsBoost;
+			const bestSetBrsBoost = bestSetInfo.traitsBonuses[0];
+
+			const finalRarity =
+				Number(currentGotchiInfo.modifiedRarityScore) +
+				bonusDifference +
+				bestSetBrsBoost;
 
 			return [
 				key,
 				{
-					id: key,
-					finalRarityScore: gotchiSetFinalRarityScore,
-					finalTraits: gotchiSetFinalTraits,
+					...currentGotchiInfo,
+					rarity: finalRarity,
+					setTraits: gotchiSetFinalTraits,
+					bestSetInfo,
 				},
 			];
-			// return []
 		}),
 	);
 
+	type Result = {
+		id: number;
+		kinship: number;
+		equippedWearables: number[];
+		experience: number;
+		baseRarityScore: number;
+		modifiedRarityScore: number;
+		modifiedNumericTraits: number[];
+		rarity: number;
+		setTraits: number[];
+	};
+
+	const tiebreaker = (
+		a: Result,
+		b: Result,
+		type: 'rarity' | 'kinship' | 'experience',
+	) => {
+		// no tiebreaker
+		if (a[type] != b[type]) return b[type] - a[type];
+		// trait tiebreaker
+		const tiebreakerIndex = round - 1;
+		return b.setTraits[tiebreakerIndex] - a.setTraits[tiebreakerIndex];
+	};
+
 	const finalRoundScores = {
 		rarity: Object.values(gotchiBestSetTraitsRarityScore)
-			.sort((a, b) => {
-				// @ts-ignore
-				return b.finalRarityScore - a.finalRarityScore;
-			})
+			.sort((a, b) => tiebreaker(a, b, 'rarity'))
 			.slice(0, winnerRange),
-		kinship: Object.values(gotchiData)
-			.sort((a, b) => {
-				// @ts-ignore
-				return b.kinship - a.kinship;
-			})
+		kinship: Object.values(gotchiBestSetTraitsRarityScore)
+			.sort((a, b) => tiebreaker(a, b, 'kinship'))
 			.slice(0, winnerRange),
-		experience: Object.values(gotchiData)
-			.sort((a, b) => {
-				// @ts-ignore
-				return b.experience - a.experience;
-			})
+		experience: Object.values(gotchiBestSetTraitsRarityScore)
+			.sort((a, b) => tiebreaker(a, b, 'experience'))
 			.slice(0, winnerRange),
 	};
 
-	log(
+	// log(
+	// 	finalRoundScores,
+	// 	finalRoundScores.rarity[0],
+	// 	gotchiBestSetTraitsRarityScore[19095],
+	// );
+
+	await jetpack.writeAsync(
+		`data/s${season}/${round}/${block_number}/${Date.now()}.json`,
 		finalRoundScores,
-		finalRoundScores.rarity[0],
-		gotchiBestSetTraitsRarityScore[19095],
 	);
 };
 
