fe371f7a54ed7cd361cf95215ebdf17a0ceb0395
candoizo <candoizo@protonmail.com>
2022-08-08 01:48:20 -0400

    docs

--- README.md
+++ README.md
@@ -4,6 +4,152 @@ aavegotchi rarity farming leaderboard
 
 <img src="https://repository-images.githubusercontent.com/521128304/f57b4194-3798-40e7-a54d-0e73ed1428cc"/>
 
-## usage
+# usage
 
-tba...
+```ts
+import { leaderboard } from '@candoizo/aavegotchi-leaderboard';
+```
+
+there is also a cli:
+
+```
+npx @candoizo/aavegotchi-leaderboard --help
+```
+
+## Functions
+
+<dl>
+<dt><a href="#isSetItemsInEquippedWearables">isSetItemsInEquippedWearables(setItems, equippedWearables)</a> ⇒ <code>boolean</code></dt>
+<dd><p>Checks whether the setItems is a subset of the equippedWearables</p>
+</dd>
+<dt><a href="#allSetsForItems">allSetsForItems(items)</a> ⇒ <code>Array.&lt;number&gt;</code></dt>
+<dd><p>This function will return all of the sets that a player has equipped based on the items they have equipped</p>
+</dd>
+<dt><a href="#bestSetOfSets">bestSetOfSets(sets)</a> ⇒ <code>number</code></dt>
+<dd><p>Takes an array of set indices and returns the index of the set with the best bonuses</p>
+</dd>
+<dt><a href="#returnRarity">returnRarity(number)</a> ⇒ <code>number</code></dt>
+<dd><p>This function returns the rarity bonus of a number given.</p>
+</dd>
+<dt><a href="#rarityScoreBonus">rarityScoreBonus(traits)</a> ⇒ <code>number</code></dt>
+<dd><p>Takes an array of trait values and returns the total rarity score bonus.</p>
+</dd>
+<dt><a href="#seasonRoundBlockSnapshots">seasonRoundBlockSnapshots(season, round)</a> ⇒ <code>number</code> | <code>undefined</code></dt>
+<dd><p>Get the block snapshots for the given season and round.</p>
+</dd>
+<dt><a href="#aavegotchis">aavegotchis([blockNumber])</a> ⇒ <code>Promise.&lt;Object&gt;</code></dt>
+<dd><p>Async function to fetch all Aavegotchi information from subgraph</p>
+</dd>
+<dt><a href="#leaderboard">leaderboard(round, [season], [blockNumber], [exportRange])</a> ⇒ <code>Promise.&lt;Object&gt;</code></dt>
+<dd><p>Returns the leaderboard for a given round and optionally a past season</p>
+</dd>
+</dl>
+
+<a name="isSetItemsInEquippedWearables"></a>
+
+## isSetItemsInEquippedWearables(setItems, equippedWearables) ⇒ <code>boolean</code>
+
+Checks whether the setItems is a subset of the equippedWearables
+
+**Kind**: global function  
+**Returns**: <code>boolean</code> - - True if the equippedWearables match the setItems
+
+| Param             | Type                              | Description                                                  |
+| ----------------- | --------------------------------- | ------------------------------------------------------------ |
+| setItems          | <code>Array.&lt;number&gt;</code> | Set to check the first array is a subset of the second array |
+| equippedWearables | <code>Array.&lt;number&gt;</code> | Set to check the first array is a subset of the second array |
+
+<a name="allSetsForItems"></a>
+
+## allSetsForItems(items) ⇒ <code>Array.&lt;number&gt;</code>
+
+This function will return all of the sets that a player has equipped based on the items they have equipped
+
+**Kind**: global function  
+**Returns**: <code>Array.&lt;number&gt;</code> - - An array of all the set ids that the player has equipped
+
+| Param | Type                              | Description                                               |
+| ----- | --------------------------------- | --------------------------------------------------------- |
+| items | <code>Array.&lt;number&gt;</code> | An array of all the item ids that the player has equipped |
+
+<a name="bestSetOfSets"></a>
+
+## bestSetOfSets(sets) ⇒ <code>number</code>
+
+Takes an array of set indices and returns the index of the set with the best bonuses
+
+**Kind**: global function  
+**Returns**: <code>number</code> - The index of the set with the best bonuses
+
+| Param | Type                              | Description             |
+| ----- | --------------------------------- | ----------------------- |
+| sets  | <code>Array.&lt;number&gt;</code> | An array of set indices |
+
+<a name="returnRarity"></a>
+
+## returnRarity(number) ⇒ <code>number</code>
+
+This function returns the rarity bonus of a number given.
+
+**Kind**: global function  
+**Returns**: <code>number</code> - The rarity of the given number.
+
+| Param  | Type                | Description                 |
+| ------ | ------------------- | --------------------------- |
+| number | <code>number</code> | A number between 1 and 100. |
+
+<a name="rarityScoreBonus"></a>
+
+## rarityScoreBonus(traits) ⇒ <code>number</code>
+
+Takes an array of trait values and returns the total rarity score bonus.
+
+**Kind**: global function  
+**Returns**: <code>number</code> - The total rarity score bonus.
+
+| Param  | Type                              | Description               |
+| ------ | --------------------------------- | ------------------------- |
+| traits | <code>Array.&lt;number&gt;</code> | An array of trait values. |
+
+<a name="seasonRoundBlockSnapshots"></a>
+
+## seasonRoundBlockSnapshots(season, round) ⇒ <code>number</code> \| <code>undefined</code>
+
+Get the block snapshots for the given season and round.
+
+**Kind**: global function  
+**Returns**: <code>number</code> \| <code>undefined</code> - A canonical snapshot block number for the given season and round. 0/undefined if not set.
+
+| Param  | Type                | Description       |
+| ------ | ------------------- | ----------------- |
+| season | <code>number</code> | The season number |
+| round  | <code>number</code> | The round number  |
+
+<a name="aavegotchis"></a>
+
+## aavegotchis([blockNumber]) ⇒ <code>Promise.&lt;Object&gt;</code>
+
+Async function to fetch all Aavegotchi information from subgraph
+
+**Kind**: global function  
+**Returns**: <code>Promise.&lt;Object&gt;</code> - - Returns data in a promise
+
+| Param         | Type                | Description                      |
+| ------------- | ------------------- | -------------------------------- |
+| [blockNumber] | <code>number</code> | The number of the block to query |
+
+<a name="leaderboard"></a>
+
+## leaderboard(round, [season], [blockNumber], [exportRange]) ⇒ <code>Promise.&lt;Object&gt;</code>
+
+Returns the leaderboard for a given round and optionally a past season
+
+**Kind**: global function  
+**Returns**: <code>Promise.&lt;Object&gt;</code> - - An object containing the leaderboard results
+
+| Param         | Type                | Description                                          |
+| ------------- | ------------------- | ---------------------------------------------------- |
+| round         | <code>number</code> | The round for which to return the leaderboard        |
+| [season]      | <code>number</code> | The season for which to return the leaderboard       |
+| [blockNumber] | <code>number</code> | The block number for which to return the leaderboard |
+| [exportRange] | <code>number</code> | The amount of results to return                      |
--- src/app.ts
+++ src/app.ts
@@ -7,29 +7,132 @@ const subgraphUrl =
 const client = new GraphQLClient(subgraphUrl);
 const SUPPLY_CAP = 25_000;
 
-export const seasonRoundBlockSnapshots = (season: number, round: number) =>
+/**
+ * @description Checks whether the setItems is a subset of the equippedWearables
+ * @param {number[]} setItems - Set to check the first array is a subset of the second array
+ * @param {number[]} equippedWearables - Set to check the first array is a subset of the second array
+ * @returns {boolean} - True if the equippedWearables match the setItems
+ */
+export const isSetItemsInEquippedWearables = (
+	setItems: number[],
+	equippedWearables: number[],
+) => {
+	// subset([1, 2, 3], [1, 2, 3, 4, 5]) // true
+	// subset([1, 2, 3, 4], [1, 2, 3]) // false
+	// subset([1, 1, 2, 2], [1, 2, 1, 2, 3]) // true
+
+	const countUnique = (arr: number[]) =>
+		arr.reduce(
+			(acc: Record<number, number>, curr: number) => ({
+				...acc,
+				[curr]: (acc[curr] || 0) + 1,
+			}),
+			{},
+		);
+
+	// Set to check the first array is a subset of the second array
+	const set1 = countUnique(setItems);
+	// Set to check the first array is a subset of the second array
+	const set2 = countUnique(equippedWearables);
+
+	for (const val of Object.keys(set1).map(Number)) {
+		// If a value doesn't exist in the second set or the key count is less
+		if (!set2[val] || set2[val] > set1[val]) {
+			// Return false since it's not a subset
+			return false;
+		}
+	}
+	return true;
+};
+
+/**
+ * @description This function will return all of the sets that a player has equipped based on the items they have equipped
+ * @param {number[]} items - An array of all the item ids that the player has equipped
+ * @returns {number[]} - An array of all the set ids that the player has equipped
+ */
+export const allSetsForItems = (items: number[]): number[] => {
+	const matchingSets = setInfo
+		.map(({ wearableIds }, index) => {
+			if (isSetItemsInEquippedWearables(wearableIds, items)) return index;
+			return 0;
+		})
+		.filter((data: number) => data != 0);
+	return matchingSets;
+};
+
+/**
+ * @description Takes an array of set indices and returns the index of the set with the best bonuses
+ * @param {number[]} sets - An array of set indices
+ * @returns {number} The index of the set with the best bonuses
+ */
+export const bestSetOfSets = (sets: number[]): number => {
+	const rawBoosts = sets.map((setIndex) => {
+		return setInfo[setIndex].traitsBonuses.reduce(
+			(curr: number, next: number) => curr + Math.abs(next),
+			0,
+		);
+	});
+
+	const findMax = (arr: number[]) => {
+		let max = 0;
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	};
+
+	const maxNum = findMax(rawBoosts);
+
+	const bestSetIndex = sets[rawBoosts.indexOf(maxNum)];
+
+	return bestSetIndex;
+};
+
+/**
+ * @description This function returns the rarity bonus of a number given.
+ * @param {number} number - A number between 1 and 100.
+ * @returns {number} The rarity of the given number.
+ */
+export const returnRarity = (number: number) => {
+	if (number < 50) return 100 - number;
+	else return number + 1;
+};
+
+/**
+ * @description Takes an array of trait values and returns the total rarity score bonus.
+ * @param {number[]} traits An array of trait values.
+ * @returns {number} The total rarity score bonus.
+ */
+export const rarityScoreBonus = (traits: number[]): number => {
+	return traits.reduce((curr, next) => curr + returnRarity(next), 0);
+};
+
+/**
+ * @description Get the block snapshots for the given season and round.
+ * @param {number} season The season number
+ * @param {number} round The round number
+ * @returns {number|undefined} A canonical snapshot block number for the given season and round. 0/undefined if not set.
+ */
+export const seasonRoundBlockSnapshots = (
+	season: number,
+	round: number,
+): number =>
 	[
 		[14082019, 14645055, 15231396, 15748551],
 		[20633778, 21170980, 21708942, 22242200],
 		[25806267, 26308346, 26854118, 27404025],
-		[0, 0, 0, 0],
+		[],
 	][season - 1][round - 1];
 
-type Re = {
-	id: number;
-	kinship: number;
-	equippedWearables: number[];
-	experience: number;
-	baseRarityScore: number;
-	modifiedRarityScore: number;
-	modifiedNumericTraits: number[];
-	name: string;
-	owner: {
-		id: string;
-	};
-};
-
-const allGotchiInfo = async (blockNumber?: number) => {
+/**
+ * @description Async function to fetch all Aavegotchi information from subgraph
+ * @async
+ * @param {number} [blockNumber] - The number of the block to query
+ * @returns {Promise<Object>} - Returns data in a promise
+ */
+export const aavegotchis = async (blockNumber?: number) => {
 	let queryStr = '';
 	for (let i = 0; i < SUPPLY_CAP / 1_000; i++) {
 		queryStr += gql` data${i}:aavegotchis(${
@@ -62,7 +165,19 @@ const allGotchiInfo = async (blockNumber?: number) => {
 			if (key.startsWith('data')) return val;
 		})
 		.filter((data) => data !== undefined)
-		.flat() as Re[];
+		.flat() as {
+		id: number;
+		kinship: number;
+		equippedWearables: number[];
+		experience: number;
+		baseRarityScore: number;
+		modifiedRarityScore: number;
+		modifiedNumericTraits: number[];
+		name: string;
+		owner: {
+			id: string;
+		};
+	}[];
 
 	return {
 		block: info._meta.block,
@@ -70,59 +185,14 @@ const allGotchiInfo = async (blockNumber?: number) => {
 	};
 };
 
-const allSetsForItems = (items: number[]): number[] => {
-	const matchingSets = setInfo
-		.map(({ wearableIds }, index) => {
-			let itemsMutable = items;
-			const includesSet = wearableIds.every((num) => {
-				if (itemsMutable.includes(num)) {
-					// remove the matches to avoid double counting
-					itemsMutable = itemsMutable.filter((item) => item != num);
-					return true;
-				}
-				return false;
-			});
-			if (includesSet) return index;
-			return 0;
-		})
-		.filter((data: number) => data != 0);
-	return matchingSets;
-};
-
-const bestSetOfSets = (sets: number[]): number => {
-	const rawBoosts = sets.map((setIndex) => {
-		return setInfo[setIndex].traitsBonuses.reduce(
-			(curr: number, next: number) => curr + Math.abs(next),
-			0,
-		);
-	});
-
-	const findMax = (arr: number[]) => {
-		let max = 0;
-		for (let i = 0; i < arr.length; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-			}
-		}
-		return max;
-	};
-
-	const maxNum = findMax(rawBoosts);
-
-	const bestSetIndex = sets[rawBoosts.indexOf(maxNum)];
-
-	return bestSetIndex;
-};
-
-const returnRarity = (number: number) => {
-	if (number < 50) return 100 - number;
-	else return number + 1;
-};
-
-const rarityScoreBonus = (traits: number[]): number => {
-	return traits.reduce((curr, next) => curr + returnRarity(next), 0);
-};
-
+/**
+ * @description Returns the leaderboard for a given round and optionally a past season
+ * @param {number} round - The round for which to return the leaderboard
+ * @param {number} [season] - The season for which to return the leaderboard
+ * @param {number} [blockNumber] - The block number for which to return the leaderboard
+ * @param {number} [exportRange] - The amount of results to return
+ * @returns {Promise<Object>} - An object containing the leaderboard results
+ */
 export const leaderboard = async (
 	round: number,
 	season?: number,
@@ -136,7 +206,7 @@ export const leaderboard = async (
 		if (newBlock) blockNumber = newBlock;
 	}
 
-	const rawResults = await allGotchiInfo(blockNumber);
+	const rawResults = await aavegotchis(blockNumber);
 
 	const gotchiData = rawResults.data;
 
@@ -222,17 +292,21 @@ export const leaderboard = async (
 		return b.setTraits[tiebreakerIndex] - a.setTraits[tiebreakerIndex];
 	};
 
+	const bestGotchisArray = Object.values(gotchiBestSetTraitsRarityScore);
+	const finalExportRange =
+		exportRange > bestGotchisArray.length ? bestGotchisArray.length : exportRange;
+
 	const finalRoundScores = {
 		block: rawResults.block,
-		rarity: Object.values(gotchiBestSetTraitsRarityScore)
+		rarity: bestGotchisArray
 			.sort((a, b) => tiebreaker(a, b, 'rarity'))
-			.slice(0, exportRange),
-		kinship: Object.values(gotchiBestSetTraitsRarityScore)
+			.slice(0, finalExportRange),
+		kinship: bestGotchisArray
 			.sort((a, b) => tiebreaker(a, b, 'kinship'))
-			.slice(0, exportRange),
-		experience: Object.values(gotchiBestSetTraitsRarityScore)
+			.slice(0, finalExportRange),
+		experience: bestGotchisArray
 			.sort((a, b) => tiebreaker(a, b, 'experience'))
-			.slice(0, exportRange),
+			.slice(0, finalExportRange),
 	};
 
 	return finalRoundScores;
